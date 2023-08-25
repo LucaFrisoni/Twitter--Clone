b49794e4be120abeb2815dd50b7b4ec7952b8bc4
LucaFrisoni <frisoniluca1@gmail.com>
2023-08-25 10:52:57 -0300

    feat:Fixing errors

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -1,6 +1,6 @@
 import prisma from "@/libs/prismaDb";
 import { NextResponse } from "next/server";
-import { useUser, useUserEmail } from "@/hooks/useUser";
+
 
 interface RequestBody {
   email: string;
@@ -10,15 +10,17 @@ interface RequestBody {
 export async function POST(request: Request) {
   try {
     const { email, body }: RequestBody = await request.json();
-    const user = await useUserEmail(email);
-    const post = await prisma.post.create({
-      data: {
-        body,
-        userId: user.id,
-      },
-    });
+    const user = await prisma.user.findUnique({ where: { email } });
+    if (user) {
+      const post = await prisma.post.create({
+        data: {
+          body,
+          userId: user.id,
+        },
+      });
 
-    return NextResponse.json(post, { status: 200 });
+      return NextResponse.json(post, { status: 200 });
+    }
   } catch (error) {
     console.log(error);
     return NextResponse.json({ error: error }, { status: 500 });
@@ -29,13 +31,15 @@ export async function GET(request: Request) {
     const { searchParams } = new URL(request.url);
     const userId = searchParams.get("userId");
     if (userId) {
-      const user = await useUser(userId);
-      const posts = await prisma.post.findMany({
-        where: { userId: user.user.id },
-        include: { user: true, comments: true },
-        orderBy: { createdAt: "desc" },
-      });
-      return NextResponse.json(posts, { status: 200 });
+      const user = await prisma.user.findUnique({ where: { id: userId } });
+      if (user) {
+        const posts = await prisma.post.findMany({
+          where: { userId: user.id },
+          include: { user: true, comments: true },
+          orderBy: { createdAt: "desc" },
+        });
+        return NextResponse.json(posts, { status: 200 });
+      }
     } else {
       const posts = await prisma.post.findMany({
         include: {
