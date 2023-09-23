37117ce7c7621913816a65a6afb15aefbb139bd8
LucaFrisoni <frisoniluca1@gmail.com>
2023-09-23 11:06:21 -0300

    Feat:HomeView Deploy&RouterBack Resolve?

--- app/components/Views/HomeView.tsx
+++ app/components/Views/HomeView.tsx
@@ -1,19 +1,22 @@
-"use client"
 import React from "react";
 import Header from "../Header";
 import Form from "../Form";
 import PostFeed from "../posts/PostFeed";
+import axios from "axios";
 
-interface HomeViewProps {
-  allPostsData: any;
-}
+// interface HomeViewProps {
+//   allPostsData: any;
+// }
 
-const HomeView = ({ allPostsData }: HomeViewProps) => {
+const HomeView = async () => {
+  const { data: allPostsDataa } = await axios.get(
+    `https://backlitter.onrender.com/posts`
+  );
   return (
     <div>
       <Header label="Home" />
       <Form placeholder="What`s happening?" />
-      <PostFeed allTweets={allPostsData} />
+      <PostFeed allTweets={allPostsDataa} />
     </div>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,10 @@
-
 import axios from "axios";
 import HomeView from "./components/Views/HomeView";
 
 const Home = async () => {
-  const { data: allPostsDataa } = await axios.get(
-    `https://backlitter.onrender.com/posts`
-  );
-
   return (
     <>
-      <HomeView allPostsData={allPostsDataa} />
+      <HomeView />
     </>
   );
 };
