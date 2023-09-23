f325b8fe858c048b58fe2f7b1a98f79b29a63aa2
LucaFrisoni <frisoniluca1@gmail.com>
2023-09-23 10:23:18 -0300

    feat:Localmente Funciona y Deploy?

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-"use client";
 import axios from "axios";
 import dynamic from "next/dynamic";
 import { useEffect, useState } from "react";
@@ -6,42 +5,34 @@ import { ClipLoader } from "react-spinners";
 import PostFeed from "./components/posts/PostFeed";
 import Header from "./components/Header";
 import Form from "./components/Form";
+import HomeView from "./components/Views/HomeView";
 
+const Home = async () => {
+  // const [isLoading, setIsLoading] = useState(true);
+  // const [allPostsData, setPost] = useState([]);
+  // const fetch = async () => {
+  const { data: allPostsDataa } = await axios.get(
+    `https://backlitter.onrender.com/posts`
+  );
+  // setPost(allPostsDataa);
+  // };
 
-
-
-const Home = () => {
-  const [isLoading, setIsLoading] = useState(true);
-  const [allPostsData, setPost] = useState([]);
-  const fetch = async () => {
-    const { data: allPostsDataa } = await axios.get(
-      `https://backlitter.onrender.com/posts`
-    );
-    setPost(allPostsDataa);
-  };
-
-
-
-
-  useEffect(() => {
-    setIsLoading(false);
-    fetch();
-  }, [fetch]);
+  // useEffect(() => {
+  //   setIsLoading(false);
+  //   fetch();
+  // }, [fetch]);
 
   return (
     <>
-      {isLoading ? ( // Renderiza el loader si isLoading es true
+      {/* {isLoading ? ( // Renderiza el loader si isLoading es true
         <div className="flex justify-center items-center">
           <ClipLoader color="lightblue" size={80} />
         </div>
-      ) : (
-        <div>
-
-          <Header label="Home" />
-          <Form placeholder="What`s happening?" />
-          <PostFeed allTweets={allPostsData} />
-        </div>
-      )}
+      ) : ( */}
+      <>
+        <HomeView allPostsData={allPostsDataa} />
+      </>
+      {/* )} */}
     </>
   );
 };
